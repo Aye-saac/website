ccf1b71846d4c1f248b29205eb19b6caaf5a6fe8
Amit Parekh <amit@dresser.app>
2020-03-30 07:49:55 +0100

    feat: add normalize.css (from styled-normalize)

--- src/index.tsx
+++ src/index.tsx
@@ -6,11 +6,14 @@ import ReactDOM from "react-dom"
 
 import { store } from "Store"
 
+import { Normalize } from "styled-normalize"
+
 import App from "./App"
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
+      <Normalize />
       <App />
     </Provider>
   </React.StrictMode>,
