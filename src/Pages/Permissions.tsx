e6ed0da19fa53e9eb269c4f5a465b861157f05f0
Amit Parekh <amit@dresser.app>
2020-03-30 16:07:40 +0100

    feat(page): add content to permission

--- src/Pages/Permissions.tsx
+++ src/Pages/Permissions.tsx
@@ -2,18 +2,24 @@ import React, { Fragment } from "react"
 
 import { Box, Button, Heading, Text } from "Components/atoms"
 
-import { Link } from "react-router-dom"
+import { Link as RouteLink } from "react-router-dom"
 
 const PermissionsPage: React.FC = () => {
   return (
     <Fragment>
       <Box variant="styles.pageWrapper">
         <Box variant="styles.container">
-          <Heading as="h2">Permissions</Heading>
-          <Text>No permissions are needed right now.</Text>
-          <Link to="/">
+          <Heading as="h2" variant="heading.h2">
+            Permissions.
+          </Heading>
+          <Text variant="primary">
+            We require some permissions in order to use the application as
+            intended. You can choose to accept any many as you want. Reasons for
+            why we need each one are listed too.
+          </Text>
+          <RouteLink to="/question">
             <Button>Continue</Button>
-          </Link>
+          </RouteLink>
         </Box>
       </Box>
     </Fragment>
