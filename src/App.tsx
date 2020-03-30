31296ec411cf6bff783cf74698ec1ed0a9b594f0
Amit Parekh <amit@dresser.app>
2020-03-30 08:02:23 +0100

    chore: replace styled-components with theme-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import React from "react"
 
-import { Navbar } from "Components/Navbar"
-
 import { BrowserRouter, Route, Switch } from "react-router-dom"
 
-import { Home } from "./pages/Home"
+import Home from "./pages/Home"
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <Navbar />
       <Switch>
         <div className="container">
           <Route path="/" component={Home} exact />
--- src/index.tsx
+++ src/index.tsx
@@ -4,18 +4,20 @@ import React from "react"
 import { Provider } from "react-redux"
 import ReactDOM from "react-dom"
 
-import { store } from "Store"
+import { ThemeProvider } from "theme-ui"
 
-import { Normalize } from "styled-normalize"
+import { store } from "Store"
+import theme from "Styles"
 
 import App from "./App"
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <Normalize />
-      <App />
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ThemeProvider>
   </React.StrictMode>,
   document.querySelector("#root"),
 )
