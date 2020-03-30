ea1e8945b1bbc3bf68502a6921da32cbc6984829
Amit Parekh <amit@dresser.app>
2020-03-30 13:18:14 +0100

    feat(component): add icon option to button

--- src/Components/atoms/Button/Button.view.tsx
+++ src/Components/atoms/Button/Button.view.tsx
@@ -1,16 +1,22 @@
 import React from "react"
 
-import { Button, ButtonProps } from "theme-ui"
+import { Box, Button, ButtonProps } from "theme-ui"
+
+import { IconType } from "react-icons"
 
 interface Props extends ButtonProps {
   children: React.ReactNode
+  IconComponent?: IconType
 }
 
-const ButtonView: React.FC<Props> = ({ children, ...props }) => {
+const NoIcon: React.FC = () => <Box sx={{ width: "1em", height: "1em" }} />
+
+const ButtonView: React.FC<Props> = ({ children, IconComponent, ...props }) => {
   return (
     <>
       <Button variant="primary" {...props}>
         {children}
+        {IconComponent ? <IconComponent /> : <NoIcon />}
       </Button>
     </>
   )
