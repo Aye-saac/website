ad2dde32c107108c43606d7218bdd485203abb67
Amit Parekh <amit@dresser.app>
2020-03-31 16:27:51 +0100

    refactor(component): move ImageWrapper style to theme

--- src/Components/atoms/ImageWrapper/ImageWrapper.view.tsx
+++ src/Components/atoms/ImageWrapper/ImageWrapper.view.tsx
@@ -1,36 +1,13 @@
 import React from "react"
 
-import { Box, SxStyleProp } from "theme-ui"
-
-const imageContainer: SxStyleProp = {
-  variant: "layout.content",
-
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "center",
-  alignItems: "center",
-
-  borderRadius: "0.75rem",
-  backgroundColor: "starWhite",
-
-  lineHeight: 1,
-
-  padding: 2,
-
-  mt: 4,
-
-  img: {
-    borderRadius: "0.5rem",
-    width: "100%",
-  },
-}
+import { Box } from "theme-ui"
 
 interface Props extends React.ImgHTMLAttributes<HTMLImageElement> {}
 
 const ImageWrapper: React.FC<Props> = ({ src, alt, ...props }) => {
   return (
     <>
-      <Box sx={imageContainer}>
+      <Box variant="image.container">
         <img src={src} alt={alt} {...props} />
       </Box>
     </>
