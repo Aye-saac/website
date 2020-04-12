29cfb164382c82c7d571a436f7933f0f6a7be621
Amit Parekh <amit@dresser.app>
2020-04-12 19:17:45 +0100

    chore: add ibm watson lib

---
+++ src/Pages/Response.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+import { Box, Section } from "Components/atoms"
+
+const ResponsePage: React.FC = () => {
+  return (
+    <>
+      <Section>
+        <Box>words</Box>
+      </Section>
+    </>
+  )
+}
+
+export default ResponsePage
